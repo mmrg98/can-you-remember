a4d1fcc7f610654197c8391c625ec305fccc264e
mmrg98 <mariam.elgaria98@gmail.com>
2020-10-07 19:09:58 +0300

    Show a single card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from "react";
 import "./App.css";
+import cards from "./data";
+import CardBack from "./images/CardBack.jpg";
 
 function App() {
-  return <div className="App border my-5">Place your code here</div>;
+  return (
+    <div className="App border my-5">
+      <div className="container">
+        <div className="row">
+          {/* 1 */}
+          <div className="col-3 my-1">
+            {/* 2 */}
+            <img
+              className="mx-auto"
+              src={CardBack}
+              //   used percentages instead of pixels to be responsive with the screen size
+              height="100%"
+              width="100%"
+              key="back"
+            />
+            {}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+ 

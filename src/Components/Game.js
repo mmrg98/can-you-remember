1fa1f5d94cb588d6726416993cceba44610a45f7
mmrg98 <mariam.elgaria98@gmail.com>
2020-10-08 06:30:08 +0300

    Done with comparing the cards and keeping a card flipped up if they match

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"; //new import
+import React, { useState, useEffect } from "react";
 
 // Data
 import allCards from "../data";
@@ -11,6 +11,7 @@ import Card from "./Card";
 
 const Game = ({ difficulty }) => {
   const [cards, setCards] = useState([]);
+  //   const [flippedCards, changeFlipped] = useState([]);
 
   //Used to duplicate the amount of cards since we need two of each and shuffle them using the function defined at the top
   useEffect(() => {
@@ -26,18 +27,41 @@ const Game = ({ difficulty }) => {
         break;
     }
     setCards(() => shuffle([...cards, ...cards]));
-  }, [difficulty]); //1
+  }, [difficulty]);
+
+  let flippedCards = [];
+  const changeFlipped = anArray => {
+    flippedCards = anArray;
+  }; //1
+
+  const unflipCards = (unflip1, unflip2) => {
+    setTimeout(() => {
+      unflip1(false);
+      unflip2(false);
+    }, 1000);
+  }; //2
+
+  const checkFlipped = flippedObject => {
+    changeFlipped([...flippedCards, flippedObject]);
+
+    if (flippedCards.length === 2) {
+      if (flippedCards[0].id !== flippedCards[1].id) {
+        unflipCards(flippedCards[0].changeFlip, flippedCards[1].changeFlip);
+      }
+      changeFlipped([]);
+    }
+  }; //3
 
   //Mapping through the array of cards and placing them in the card component
-  const cardsGrid = cards.map((card, idx) => (
-    <Card key={`${card.id}-${idx}`} card={card} />
+  const cardList = cards.map((card, idx) => (
+    <Card key={`${card.id}-${idx}`} card={card} checkFlipped={checkFlipped} /> //4
   ));
 
   return (
     <div className="container">
       <div className="row">
         <div className=" col-9">
-          <div className="row border">{cardsGrid}</div>
+          <div className="row border">{cardList}</div>
         </div>
       </div>
     </div>
